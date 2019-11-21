e813af51d5886a8b0652030f7bdc93cb62d9144a
Stefano <stefano.savare@gmail.com>
2019-11-21 18:41:00 +0100

    Added time series transformer first child class
    Signed-off-by: Stefano <stefano.savare@gmail.com>

--- giottotime/features/features_transformation/base.py
+++ giottotime/features/features_transformation/base.py
@@ -1,10 +1,21 @@
 import inspect
 from abc import ABCMeta, abstractmethod
+from typing import List, Union
 
+
+ColumnNames = Union[List[str], str]
 
 class TimeSeriesTransformer(metaclass=ABCMeta):
-    def __init__(self):
-        pass
+    def __init__(self, input_columns: ColumnNames, output_columns: ColumnNames,
+                 drop_input_columns: bool = False):
+        self._check_input(input_columns, output_columns, drop_input_columns)
+        self.input_columns = input_columns
+        if isinstance(input_columns, str):
+            self.input_columns = [input_columns]
+        self.output_columns = output_columns
+        if isinstance(input_columns, str):
+            self.input_columns = [input_columns]
+        self.drop_input_columns = drop_input_columns
 
     def fit(self, X, y=None):
         return self
@@ -17,9 +28,29 @@ class TimeSeriesTransformer(metaclass=ABCMeta):
         self.fit(X, y)
         return self.transform(X)
 
+    def _check_input(self, input_columns: ColumnNames,
+                     output_columns: ColumnNames, drop_input_columns: bool):
+        self._check_columns(input_columns, 'input_columns')
+        self._check_columns(output_columns, 'output_columns')
+        self._check_drop_input_columns(drop_input_columns)
+
+    def _check_columns(self, columns: ColumnNames, attribute_name: str):
+        if isinstance(columns, str):
+            return
+        elif isinstance(columns, list):
+            if len(columns) == 0:
+                raise ValueError(f'{attribute_name} is empty')
+        else:
+            raise TypeError(f'{attribute_name} must be a string or a list')
+
+    def _check_drop_input_columns(self, drop_input_columns: bool):
+        if not isinstance(drop_input_columns, bool):
+            raise TypeError('drop_input_columns must be a boolean')
+
     def __repr__(self):
         constructor_attributes = inspect.getfullargspec(self.__init__).args
-        attributes_to_print = [str(getattr(self, attribute)) for attribute in constructor_attributes
+        attributes_to_print = [str(getattr(self, attribute))
+                               for attribute in constructor_attributes
                                if attribute in self.__dict__]
         return "{class_name}({attributes})".format(class_name=self.__class__.__name__,
                                                    attributes=", ".join(attributes_to_print))
--- giottotime/features/features_transformation/time_series_transformers.py
+++ giottotime/features/features_transformation/time_series_transformers.py
@@ -1,10 +1,29 @@
+from typing import List, Union
+
+from .base import TimeSeriesTransformer
 
 
-class ColumnsProduct:
+ColumnNames = Union[List[str], str]
 
-    def __init__(self, column1, column2):
-        self.column1 = column1
-        self.column2 = column2
+
+class ColumnsProduct(TimeSeriesTransformer):
+
+    def __init__(
+            self,
+            input_columns: ColumnNames,
+            output_columns: ColumnNames
+    ):
+        super().__init__(input_columns, output_columns, False)
+        self.output_column = output_columns[0]
 
     def transform(self, X):
-        return X[self.column1] * X[self.column2]
+        return X[self.input_columns].prod(axis=1)
+
+    def _check_input(self, input_columns: List[str], output_columns: List[str],
+                     drop_input_columns: bool):
+        super(self)._check_input(input_columns, output_columns,
+                                 self.drop_input_columns)
+        if len(output_columns) != 1:
+            raise ValueError('output_columns contains more than one column')
+
+
