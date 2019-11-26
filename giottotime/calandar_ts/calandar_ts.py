14fdac98ac2387925eb84288adcbdca1b6bdc3d4
Benjamin Russell <benjaminrussell@philippes-mbp.lan>
2019-11-26 15:09:41 +0100

    created event feature creation functions with custom kernel option

---
+++ giottotime/calandar_ts/calandar_ts.py
@@ -0,0 +1,72 @@
+from datetime import date, datetime
+
+import pandas as pd
+import importlib
+
+import workalendar
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+from scipy import signal
+
+###########################################################################
+
+year = datetime.now().year
+
+###########################################################################
+
+def holiday_ts(region="america", country="Brazil", start_date='01/01/2018', end_date='01/01/2020', kernel="none"):
+    mod = importlib.import_module(f".{region}", f"workalendar")
+    country_mod = getattr(mod, country)()
+
+    index = pd.date_range(start=start_date, end=end_date, freq='D')
+
+    years = index.year.unique()
+
+    events = pd.DataFrame()
+
+    for year in years:
+        events = events.append( country_mod.holidays(year) )
+
+    events = events.rename(columns={0 : "date", 1 : "events"})
+    events['date'] = pd.to_datetime(events['date'])
+    events['status'] = 1
+    events = events.set_index('date')
+
+    events = events.reindex(index)
+
+    events['events'] = events['events'].fillna("none")
+    events['status'] = events['status'].fillna(0).astype(int)
+
+    events = events.sort_index()
+
+    klen = len(kernel)
+
+    if kernel == "none":
+        return events
+    else:
+        def ip(w):
+            if sum(w) != 0:
+                return np.dot(w, kernel)/sum(w)
+            else:
+                return 0
+
+        events['status'] = events['status'].rolling(klen, center=True).apply( ip )
+
+        return events
+
+###########################################################################
+
+if __name__ == "__main__":
+
+    #kernel = list( range(1,11) ) + list( range(-10,1) )
+
+    kernel = list( range(1,11) ) + list( range(-10,1) ) #signal.nuttall(12) #list( range(1, 11) )
+
+    e = holiday_ts(region="europe", country="Iceland", kernel="kernel" )
+    print(e)
+
+    e.plot()
+    plt.show()
