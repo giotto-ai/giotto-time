ebe2838b6cdb71fed31329e845338b226cbe4550
Alessio Baccelli <alessio.baccelli95@gmail.com>
2019-11-28 15:41:43 +0100

    Tda features and docstrings  (#4)
    * Added TDA feature
    
    Signed-off-by: Stefano <stefano.savare@gmail.com>
    
    * Renamed majuscules
    
    Signed-off-by: Stefano <stefano.savare@gmail.com>
    
    * Refactor of tda features
    
    * First draft of feature creation
    
    * Some docstrings for TDA features added
    
    * Added docstrings
    
    * Changed naming conventions for features and docstrings added
    
    * Fixed bug in column naming
    
    * TDA features added
    
    * Refactor of docstrings
    
    * Docstrings added
    
    * Refactor of method
    
    * Refactor

--- giottotime/features/features_creation/feature_creation.py
+++ giottotime/features/features_creation/feature_creation.py
@@ -1,41 +1,99 @@
+from typing import List
+
 import pandas as pd
 
-from giottotime.feature_creation.time_series_features import ShiftFeature
-from giottotime.feature_creation.utils import get_train_test_features
+from giottotime.features.features_creation.base import TimeSeriesFeature
+from giottotime.features.features_creation.time_series_features import ShiftFeature
+from giottotime.features.utils import get_non_nan_values
 
 
 class FeaturesCreation:
-    """
-    Tentative docstring
+    """Class responsible for the generation of the features, starting from a
+    list of ``TimeSeriesFeature``.
+
+    Parameters
+    ----------
+    horizon : ``int``, required.
+        It represents how much into the future is necessary to predict. This
+        corresponds to the number of shifts that are going to be performed
+        on y.
+
+    time_series_features : ``List[TimeSeriesFeature]``, required.
+        The list of ``TimeSeriesFeature`` from which to compute the features.
 
     """
-    def __init__(self, horizon, time_series_features):
-        if horizon <= 0 or not isinstance(horizon, int):
-            raise ValueError("The horizon should be an int greater than 0, "
-                             f"but has value {horizon}.")
-
-        self.horizon = horizon
+    def __init__(self, horizon: int,
+                 time_series_features: List[TimeSeriesFeature]):
         self.time_series_features = time_series_features
+        self.horizon = horizon
 
-    def _create_y_shifts(self, time_series):
+    def fit_transform(self, time_series: pd.DataFrame) \
+            -> (pd.DataFrame, pd.DataFrame):
+        """Create the X matrix by generating the features, starting from the
+        original ``time_series`` and using the list of ``time_series_features``.
+        Also create the y matrix, by generating ``horizon`` number of shifts
+        of the ``time_series``.
+        Rows of ``X`` that contain at least a ``Nan`` value are
+        dropped, as well as the corresponding rows of ``y``.
+        Rows of ``y`` that contain a Nan are instead keep, since are going to
+        be used in later steps as prediction time.
+
+        Parameters
+        ----------
+        time_series : ``pd.DataFrame``, required.
+            The time-series on which to compute the ``X`` and ``y`` matrices.
+
+        Returns
+        -------
+        x_non_nans, y_non_nans : ``(pd.DataFrame, pd.DataFrame)``
+            A tuple containing the ``X`` and ``y`` matrices.
+
+        """
+        x = self._create_x_features(time_series)
+        y = self._create_y_shifts(time_series)
+        x_non_nans, y_non_nans = get_non_nan_values(x, y)
+        return x_non_nans, y_non_nans
+
+    def _create_y_shifts(self, time_series: pd.DataFrame) -> pd.DataFrame:
+        """Generate ``n`` shifts of ``time_series``, where ``n`` is equal to
+        the ``horizon``.
+
+        Parameters
+        ----------
+        time_series : ``pd.DataFrame``, required.
+            The original DataFrame on which to generate the shifts.
+
+        Returns
+        -------
+        y_shifted : ``pd.DataFrame``
+            The DataFrame containing the shifts of ``time_series``.
+
+        """
         y = pd.DataFrame(index=time_series.index)
         for k in range(self.horizon):
-            shift_feature = ShiftFeature(-k)
-            y[f"y_{k}"] = shift_feature.fit_transform(time_series)
+            shift_feature = ShiftFeature(-k, f'shift_{k}')
+            y[f'y_{k}'] = shift_feature.fit_transform(time_series)
 
         return y
 
-    def _create_x_features(self, time_series):
-        x = pd.DataFrame(index=time_series.index)
+    def _create_x_features(self, time_series: pd.DataFrame) -> pd.DataFrame:
+        """Create a DataFrame, containing a set of features generated from the
+        ``time_series`` and using the ``time_series_features``.
+
+        Parameters
+        ----------
+        time_series : ``pd.DataFrame``, required.
+            The original DataFrame on which to generate the shifts.
+
+        Returns
+        -------
+        features : ``pd.DataFrame``
+            The DataFrame containing the features.
+
+        """
+        features = pd.DataFrame(index=time_series.index)
         for time_series_feature in self.time_series_features:
-            x[str(time_series_feature)] = time_series_feature.fit_transform(time_series)
+            x_trasformed = time_series_feature.fit_transform(time_series)
+            features = pd.concat([features, x_trasformed], axis=1)
 
-        return x
-
-    def fit_transform(self, time_series):
-        x = self._create_x_features(time_series)
-        y = self._create_y_shifts(time_series)
-
-        train_x, train_y, test_x = get_train_test_features(x, y)
-
-        return train_x, train_y, test_x
+        return features
