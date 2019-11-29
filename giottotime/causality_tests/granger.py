d2c34cc6c7e3f0e9adbf228dca8f4f4b79b15669
alexbacce <alessio.baccelli95@gmail.com>
2019-11-29 10:36:58 +0100

    Refactor

--- giottotime/causality_tests/granger.py
+++ giottotime/causality_tests/granger.py
@@ -1,19 +1,11 @@
-from sklearn.metrics import mean_squared_error
-
-import pandas.util.testing as testing
+from itertools import product
 
 import numpy as np
 import pandas as pd
-
-from itertools import product
-
-from math import sqrt
-
 from sklearn.linear_model import LinearRegression
 
-################################################################################################
 
-class GrangerTest():
+class GrangerTest:
     def __init__(self, data):
         self.data = data
 
@@ -31,21 +23,23 @@ class GrangerTest():
     def all_best_corr_shifts(self, max_shift):
         self.best_shifts = pd.DataFrame()
 
-        for x, y in product( self.data.columns, repeat=2 ):
-            res = self.get_max_corr_shift( max_shift, x=x, y=y )
+        for x, y in product(self.data.columns, repeat=2):
+            res = self.get_max_corr_shift(max_shift, x=x, y=y)
 
             best_shift = res[1]
             max_corr = res[0]
             N = self.data.shape[0] - max_shift
 
-            self.best_shifts = self.best_shifts.append( {   'x' : x ,
-                                                            'y' : y,
-                                                            'shift' : best_shift,
-                                                            'max_corr' : max_corr
-                                                            }, ignore_index = True )
+            self.best_shifts = self.best_shifts.append({'x': x,
+                                                        'y': y,
+                                                        'shift': best_shift,
+                                                        'max_corr': max_corr
+                                                        }, ignore_index=True)
 
-        best_shifts = pd.pivot_table( self.best_shifts, index=['x'], columns=['y'], values='shift' )
-        max_corrs = pd.pivot_table( self.best_shifts, index=['x'], columns=['y'], values='max_corr' )
+        best_shifts = pd.pivot_table(self.best_shifts, index=['x'],
+                                     columns=['y'], values='shift')
+        max_corrs = pd.pivot_table(self.best_shifts, index=['x'],
+                                   columns=['y'], values='max_corr')
 
         return best_shifts, max_corrs
 
@@ -64,21 +58,23 @@ class GrangerTest():
     def all_best_coeff_shifts(self, max_shift):
         self.best_shifts = pd.DataFrame()
 
-        for x, y in product( self.data.columns, repeat=2 ):
+        for x, y in product(self.data.columns, repeat=2):
             res = self.get_max_coeff_shift(max_shift, x=x, y=y)
 
             best_shift = res[1]
             max_corr = res[0]
             N = self.data.shape[0] - max_shift
 
-            self.best_shifts = self.best_shifts.append( {   'x' : x ,
-                                                            'y' : y,
-                                                            'shift' : best_shift,
-                                                            'max_corr' : max_corr
-                                                            }, ignore_index = True )
+            self.best_shifts = self.best_shifts.append({'x': x,
+                                                        'y': y,
+                                                        'shift': best_shift,
+                                                        'max_corr': max_corr
+                                                        }, ignore_index=True)
 
-        best_shifts = pd.pivot_table( self.best_shifts, index=['x'], columns=['y'], values='shift' )
-        max_corrs = pd.pivot_table( self.best_shifts, index=['x'], columns=['y'], values='max_corr' )
+        best_shifts = pd.pivot_table(self.best_shifts, index=['x'],
+                                     columns=['y'], values='shift')
+        max_corrs = pd.pivot_table(self.best_shifts, index=['x'],
+                                   columns=['y'], values='max_corr')
 
         return best_shifts, max_corrs
 
@@ -92,37 +88,40 @@ class GrangerTest():
             shifts[shift] = self.data[x].shift(shift)
 
         shifts = shifts.dropna()
-        lf = LinearRegression().fit( shifts[range(max_shift)].values, shifts[y].values )
+        lf = LinearRegression().fit(shifts[range(max_shift)].values,
+                                    shifts[y].values)
 
         q = lf.coef_.max(), np.argmax(lf.coef_)
         return q
 
-################################################################################################
+
 
 if __name__ == "__main__":
     import pandas.util.testing as testing
-    import matplotlib.pyplot as plt
     from random import randint
 
     testing.N, testing.K = 2000, 1
 
     df = testing.makeTimeDataFrame(freq='MS')
-    df['B'] = df['A'].shift(8) + pd.Series(data=np.random.normal( 0, 0.3, df.shape[0] ), index=df.index)
+    df['B'] = df['A'].shift(8) + pd.Series(
+        data=np.random.normal(0, 0.3, df.shape[0]), index=df.index)
 
     for k in range(10):
-        df[f'C_{k}'] = df['A'].shift(8 + randint(10, 20)) + pd.Series(data=np.random.normal( 0, 0.3, df.shape[0] ), index=df.index)
+        df[f'C_{k}'] = df['A'].shift(8 + randint(10, 20)) + pd.Series(
+            data=np.random.normal(0, 0.3, df.shape[0]), index=df.index)
 
     df = df.dropna()
 
     gt = GrangerTest(df)
-    #q = gt.shifted_partial_corr_matrix(20, 'A', 'B')
+    # q = gt.shifted_partial_corr_matrix(20, 'A', 'B')
 
     w = gt.all_best_corr_shifts(max_shift=20)
     print(w[0])
     print("\n\n±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±\n\n")
     print(w[1])
 
-    print("\n\n±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±\n\n")
+    print(
+        "\n\n±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±\n\n")
 
     q = gt.all_best_coeff_shifts(max_shift=20)
 
