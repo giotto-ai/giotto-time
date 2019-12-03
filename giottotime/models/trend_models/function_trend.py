87badad61e95d44e83c590a94443234f00bd6c2c
Benjamin Russell <benjaminrussell@philippes-mbp.lan>
2019-12-03 19:02:48 +0100

    added doc strings for trend models

--- giottotime/models/trend_models/function_trend.py
+++ giottotime/models/trend_models/function_trend.py
@@ -6,8 +6,6 @@ import pandas as pd
 
 from giottotime.models.trend_models.base import TrendModel
 
-
-# TODO: finish this class
 class FunctionTrend(TrendModel):
     """A model for fitting, predicting and removing an custom functional trend
     from a time series. The transformed time series created will be trend
@@ -25,7 +23,29 @@ class FunctionTrend(TrendModel):
         self.model_form = model_form
         self.loss = loss
 
-    def fit(self, time_series, x0, method="BFGS"):
+    def fit(self, time_series: pd.DataFrame, x0: list, method: str = 'BFGS') \
+        -> TrendModel:
+        """Fit the model on the ``time_series``, with respect to the provided
+        ``loss`` and using the provided ``method``. In order to see which
+        methods are available, please check the 'scipy' `documentation
+        <https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html>`_.
+
+        Parameters
+        ----------
+        time_series : ``pd.DataFrame``, required.
+            The time series on which to fit the model.
+
+        x0 : ``list``.
+
+        method : ``str``, optional, (default=``'BFGS``).
+            The method to use in order to minimize the loss function.
+
+        Returns
+        -------
+        self : ``TrendModel``
+            The fitted object.
+
+        """
         def prediction_error(model_weights):
             predictions = [self.model_form(t, model_weights) for t in
                            range(0, time_series.shape[0])]
@@ -38,10 +58,41 @@ class FunctionTrend(TrendModel):
         return self
 
     def predict(self, t):
+        """Using the fitted polynomial, predict the values starting from ``X``.
+
+        Parameters
+        ----------
+        X : ``pd.DataFrame``, required.
+            The time series on which to predict.
+
+        Returns
+        -------
+        predictions : ``pd.DataFrame``
+            The output predictions.
+
+        Raises
+        ------
+        ``NotFittedError``
+            Raised if the model is not fitted yet.
+
+        """
         # check fit run
         return self.model_form(t, self.model_weights_)
 
     def transform(self, time_series):
+        """Transform the ``time_series`` by removing the trend.
+
+        Parameters
+        ----------
+        time_series : ``pd.DataFrame``, required.
+            The time series to transform.
+
+        Returns
+        -------
+        transformed_time_series : ``pd.DataFrame``
+            The transformed time series, without the trend.
+
+        """
         # check fit run
         predictions = pd.DataFrame(index=time_series.index, data=[
             self.model_form(t, self.model_weights_) for t in
