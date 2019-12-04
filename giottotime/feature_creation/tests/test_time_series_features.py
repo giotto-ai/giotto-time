24d9095ab8193c909da28a6f56ddacaeeaa99239
Alessio Baccelli <alessio.baccelli95@gmail.com>
2019-12-04 16:32:23 +0100

    Polynomial features #16 and unittest added (#25)

--- giottotime/feature_creation/tests/test_time_series_features.py
+++ giottotime/feature_creation/tests/test_time_series_features.py
@@ -1,12 +1,13 @@
-import pandas as pd
 import numpy as np
-import pytest
+import pandas as pd
 import pandas.util.testing as testing
-from hypothesis import given, strategies as st, settings
+import pytest
+from hypothesis import given, strategies as st
+from sklearn.preprocessing import PolynomialFeatures
 
 from giottotime.core.hypothesis.time_indexes import giotto_time_series
 from giottotime.feature_creation import ShiftFeature, MovingAverageFeature, \
-    ConstantFeature, ExogenousFeature, CustomFeature
+    ConstantFeature, ExogenousFeature, CustomFeature, PolynomialFeature
 
 
 class TestShiftFeature:
@@ -15,7 +16,7 @@ class TestShiftFeature:
 
     def test_forward_time_shift_feature(self):
         output_name = 'shift'
-        df = pd.DataFrame.from_dict({output_name: [0, 1, 2, 3, 4, 5]})
+        df = pd.DataFrame.from_dict({'old_name': [0, 1, 2, 3, 4, 5]})
 
         shift_feature = ShiftFeature(shift=1, output_name=output_name)
         expected_df = pd.DataFrame.from_dict(
@@ -26,7 +27,7 @@ class TestShiftFeature:
 
     def test_backwards_time_shift_feature(self):
         output_name = 'shift'
-        df = pd.DataFrame.from_dict({output_name: [0, 1, 2, 3, 4, 5]})
+        df = pd.DataFrame.from_dict({'old_name': [0, 1, 2, 3, 4, 5]})
 
         shift_feature = ShiftFeature(shift=-2, output_name=output_name)
         expected_df = pd.DataFrame.from_dict(
@@ -37,10 +38,23 @@ class TestShiftFeature:
 
     def test_zero_shift_feature(self):
         output_name = 'shift'
-        df = pd.DataFrame.from_dict({output_name: [0, 1, 2, 3, 4, 5]})
+        df = pd.DataFrame.from_dict({'old_name': [0, 1, 2, 3, 4, 5]})
 
         shift_feature = ShiftFeature(shift=0, output_name=output_name)
         expected_df = pd.DataFrame.from_dict({output_name: [0, 1, 2, 3, 4, 5]})
+
+        df_shifted = shift_feature.fit_transform(df)
+        testing.assert_frame_equal(expected_df, df_shifted)
+
+    def test_multi_columns_time_shift_feature(self):
+        output_name = 'shift'
+        df = pd.DataFrame.from_dict({'old_name_0': [0, 1, 2, 3, 4, 5],
+                                     'old_name_1': [7, 8, 9, 10, 11, 12]})
+
+        shift_feature = ShiftFeature(shift=-2, output_name=output_name)
+        expected_df = pd.DataFrame.from_dict(
+            {f'{output_name}_0': [2, 3, 4, 5, np.nan, np.nan],
+             f'{output_name}_1': [9, 10, 11, 12, np.nan, np.nan]})
 
         df_shifted = shift_feature.fit_transform(df)
         testing.assert_frame_equal(expected_df, df_shifted)
@@ -66,7 +80,7 @@ class TestMovingAverageFeature:
     def test_invalid_window_size(self):
         output_name = 'moving_average'
         window_size = -1
-        df = pd.DataFrame.from_dict({output_name: [0, 1, 2, 3, 4, 5]})
+        df = pd.DataFrame.from_dict({'old_name': [0, 1, 2, 3, 4, 5]})
 
         ma_feature = MovingAverageFeature(window_size=window_size,
                                           output_name=output_name)
@@ -76,13 +90,28 @@ class TestMovingAverageFeature:
     def test_positive_window_size(self):
         output_name = 'moving_average'
         window_size = 2
-        df = pd.DataFrame.from_dict({output_name: [0, 1, 2, 3, 4, 5]})
+        df = pd.DataFrame.from_dict({'old_name': [0, 1, 2, 3, 4, 5]})
 
         ma_feature = MovingAverageFeature(window_size=window_size,
                                           output_name=output_name)
         df_ma = ma_feature.fit_transform(df)
         expected_df_ma = pd.DataFrame.from_dict(
             {output_name: [np.nan, np.nan, 0.5, 1.5, 2.5, 3.5]})
+
+        testing.assert_frame_equal(expected_df_ma, df_ma)
+
+    def test_multi_columns_window_size(self):
+        output_name = 'moving_average'
+        window_size = 2
+        df = pd.DataFrame.from_dict({'old_name_0': [0, 1, 2, 3, 4, 5],
+                                     'old_name_1': [7, 8, 9, 10, 11, 12]})
+
+        ma_feature = MovingAverageFeature(window_size=window_size,
+                                          output_name=output_name)
+        df_ma = ma_feature.fit_transform(df)
+        expected_df_ma = pd.DataFrame.from_dict(
+            {f'{output_name}_0': [np.nan, np.nan, 0.5, 1.5, 2.5, 3.5],
+             f'{output_name}_1': [np.nan, np.nan, 7.5, 8.5, 9.5, 10.5]})
 
         testing.assert_frame_equal(expected_df_ma, df_ma)
 
@@ -112,7 +141,7 @@ class TestConstantFeature:
     def test_correct_constant_feature(self):
         output_name = 'constant_feature'
         constant = 12
-        df = pd.DataFrame.from_dict({output_name: [0, 1, 2, 3, 4, 5]})
+        df = pd.DataFrame.from_dict({'old_name': [0, 1, 2, 3, 4, 5]})
 
         constant_feature = ConstantFeature(constant=constant,
                                            output_name=output_name)
@@ -148,13 +177,13 @@ class TestExogenousFeature:
     def test_correct_exog_none_method(self):
         output_name = 'exog'
         method = None
-        exog = pd.DataFrame.from_dict({output_name: [0, 1, 2, 3]})
+        exog = pd.DataFrame.from_dict({'old_name_1': [0, 1, 2, 3]})
         exog.index = ([pd.Timestamp(2000, 1, 1),
                        pd.Timestamp(2000, 2, 1),
                        pd.Timestamp(2000, 3, 1),
                        pd.Timestamp(2000, 4, 1)])
 
-        df = pd.DataFrame.from_dict({output_name: [10, 11, 12, 13]})
+        df = pd.DataFrame.from_dict({'old_name_2': [10, 11, 12, 13]})
         df.index = ([pd.Timestamp(2000, 1, 1),
                      pd.Timestamp(2000, 1, 2),
                      pd.Timestamp(2000, 1, 3),
@@ -179,13 +208,13 @@ class TestExogenousFeature:
     def test_correct_exog_backfill_method(self):
         output_name = 'exog'
         method = 'backfill'
-        exog = pd.DataFrame.from_dict({output_name: [0, 1, 2, 3]})
+        exog = pd.DataFrame.from_dict({'old_name_1': [0, 1, 2, 3]})
         exog.index = ([pd.Timestamp(2000, 1, 1),
                        pd.Timestamp(2000, 2, 1),
                        pd.Timestamp(2000, 3, 1),
                        pd.Timestamp(2000, 4, 1)])
 
-        df = pd.DataFrame.from_dict({output_name: [10, 11, 12, 13]})
+        df = pd.DataFrame.from_dict({'old_name_2': [10, 11, 12, 13]})
         df.index = ([pd.Timestamp(2000, 1, 1),
                      pd.Timestamp(2000, 1, 2),
                      pd.Timestamp(2000, 1, 3),
@@ -207,13 +236,13 @@ class TestExogenousFeature:
     def test_correct_exog_pad_method(self):
         output_name = 'exog'
         method = 'pad'
-        exog = pd.DataFrame.from_dict({output_name: [0, 1, 2, 3]})
+        exog = pd.DataFrame.from_dict({'old_name_1': [0, 1, 2, 3]})
         exog.index = ([pd.Timestamp(2000, 1, 1),
                        pd.Timestamp(2000, 2, 1),
                        pd.Timestamp(2000, 3, 1),
                        pd.Timestamp(2000, 4, 1)])
 
-        df = pd.DataFrame.from_dict({output_name: [10, 11, 12, 13]})
+        df = pd.DataFrame.from_dict({'old_name_2': [10, 11, 12, 13]})
         df.index = ([pd.Timestamp(2000, 1, 1),
                      pd.Timestamp(2000, 1, 2),
                      pd.Timestamp(2000, 1, 3),
@@ -235,13 +264,13 @@ class TestExogenousFeature:
     def test_correct_nearest_pad_method(self):
         output_name = 'exog'
         method = 'nearest'
-        exog = pd.DataFrame.from_dict({output_name: [0, 1, 2, 3]})
+        exog = pd.DataFrame.from_dict({'old_name_1': [0, 1, 2, 3]})
         exog.index = ([pd.Timestamp(2000, 1, 1),
                        pd.Timestamp(2000, 2, 1),
                        pd.Timestamp(2000, 3, 1),
                        pd.Timestamp(2000, 4, 1)])
 
-        df = pd.DataFrame.from_dict({output_name: [10, 11, 12, 13]})
+        df = pd.DataFrame.from_dict({'old_name_2': [10, 11, 12, 13]})
         df.index = ([pd.Timestamp(2000, 1, 1),
                      pd.Timestamp(2000, 1, 2),
                      pd.Timestamp(2000, 1, 3),
@@ -260,28 +289,130 @@ class TestExogenousFeature:
 
         testing.assert_frame_equal(expected_exog, new_exog_feature)
 
+    def test_correct_multi_columns_exog(self):
+        output_name = 'exog'
+        exog = pd.DataFrame.from_dict({'old_name_0': [0, 1, 2, 3],
+                                       'old_name_1': [5, 6, 7, 8]})
+        exog.index = ([pd.Timestamp(2000, 1, 1),
+                       pd.Timestamp(2000, 2, 1),
+                       pd.Timestamp(2000, 3, 1),
+                       pd.Timestamp(2000, 4, 1)])
+
+        df = pd.DataFrame.from_dict({'old_name_2': [10, 11, 12, 13]})
+        df.index = ([pd.Timestamp(2000, 1, 1),
+                     pd.Timestamp(2000, 1, 2),
+                     pd.Timestamp(2000, 1, 3),
+                     pd.Timestamp(2000, 1, 29)])
+
+        exog_feature = ExogenousFeature(exogenous_time_series=exog,
+                                        output_name=output_name)
+
+        new_exog_feature = exog_feature.fit_transform(df)
+        expected_exog = pd.DataFrame.from_dict(
+            {f'{output_name}_0': [0, np.nan, np.nan, np.nan],
+             f'{output_name}_1': [5, np.nan, np.nan, np.nan]})
+        expected_exog.index = ([pd.Timestamp(2000, 1, 1),
+                                pd.Timestamp(2000, 1, 2),
+                                pd.Timestamp(2000, 1, 3),
+                                pd.Timestamp(2000, 1, 29)])
+
+        testing.assert_frame_equal(expected_exog, new_exog_feature)
+
 
 class TestPolynomialFeature:
-    pass
+    def _correct_pol_features(self, df: pd.DataFrame, degree: int) \
+            -> pd.DataFrame:
+        poly = PolynomialFeatures(degree)
+        pol_features_arr = poly.fit_transform(df)
+        pol_features = pd.DataFrame(pol_features_arr, index=df.index)
+        return pol_features
+
+    def test_correct_pol_features_single_column(self):
+        output_name = 'pol_features'
+        degree = 3
+        df = pd.DataFrame.from_dict({'old_name': [0, 1, 2, 3]})
+        df.index = ([pd.Timestamp(2000, 1, 1),
+                     pd.Timestamp(2000, 2, 1),
+                     pd.Timestamp(2000, 3, 1),
+                     pd.Timestamp(2000, 4, 1)])
+
+        pol_feature = PolynomialFeature(degree=degree, output_name=output_name)
+        pol_df = pol_feature.fit_transform(df)
+
+        expected_pol_df = pd.DataFrame.from_dict(
+            {f'{output_name}_0': [1., 1., 1., 1.],
+             f'{output_name}_1': [0., 1., 2., 3.],
+             f'{output_name}_2': [0., 1., 4., 9.],
+             f'{output_name}_3': [0., 1., 8., 27.]})
+        expected_pol_df.index = ([pd.Timestamp(2000, 1, 1),
+                                  pd.Timestamp(2000, 2, 1),
+                                  pd.Timestamp(2000, 3, 1),
+                                  pd.Timestamp(2000, 4, 1)])
+
+        testing.assert_frame_equal(expected_pol_df, pol_df)
+
+    def test_correct_pol_features_multi_columns(self):
+        output_name = 'pol_features'
+        degree = 2
+        df = pd.DataFrame.from_dict({'old_name_1': [0, 2, 4],
+                                     'old_name_2': [1, 3, 5]})
+        df.index = ([pd.Timestamp(2000, 1, 1),
+                     pd.Timestamp(2000, 2, 1),
+                     pd.Timestamp(2000, 3, 1)])
+
+        pol_feature = PolynomialFeature(degree=degree, output_name=output_name)
+        pol_df = pol_feature.fit_transform(df)
+
+        expected_pol_df = pd.DataFrame.from_dict(
+            {f'{output_name}_0': [1., 1., 1],
+             f'{output_name}_1': [0., 2., 4],
+             f'{output_name}_2': [1., 3., 5],
+             f'{output_name}_3': [0., 4., 16.],
+             f'{output_name}_4': [0., 6., 20.],
+             f'{output_name}_5': [1., 9., 25.]})
+        expected_pol_df.index = ([pd.Timestamp(2000, 1, 1),
+                                  pd.Timestamp(2000, 2, 1),
+                                  pd.Timestamp(2000, 3, 1)])
+
+        testing.assert_frame_equal(expected_pol_df, pol_df)
 
 
 class TestCustomFeature:
-    def df_to_power(self, df: pd.DataFrame, power: int) -> pd.DataFrame:
+    def _df_to_power(self, df: pd.DataFrame, power: int) -> pd.DataFrame:
         return np.power(df, power)
 
     def test_correct_custom_feature(self):
         output_name = 'custom'
         power = 3
-        df = pd.DataFrame.from_dict({output_name: [0, 1, 2, 3, 4, 5]})
+        df = pd.DataFrame.from_dict({'old_name': [0, 1, 2, 3, 4, 5]})
 
         custom_feature = CustomFeature(
-            custom_feature_function=self.df_to_power,
+            custom_feature_function=self._df_to_power,
             output_name=output_name,
             power=power)
 
         output_custom_feature = custom_feature.fit_transform(df)
         expected_custom_output = pd.DataFrame.from_dict(
             {output_name: [0, 1, 8, 27, 64, 125]})
+
+        testing.assert_frame_equal(expected_custom_output,
+                                   output_custom_feature)
+
+    def test_multi_columns_custom_feature(self):
+        output_name = 'custom'
+        power = 2
+        df = pd.DataFrame.from_dict({'old_name': [0, 1, 2, 3, 4, 5],
+                                     'old_name_1': [7, 8, 9, 10, 11, 12]})
+
+        custom_feature = CustomFeature(
+            custom_feature_function=self._df_to_power,
+            output_name=output_name,
+            power=power)
+
+        output_custom_feature = custom_feature.fit_transform(df)
+        expected_custom_output = pd.DataFrame.from_dict(
+            {f'{output_name}_0': [0, 1, 4, 9, 16, 25],
+             f'{output_name}_1': [49, 64, 81, 100, 121, 144]})
 
         testing.assert_frame_equal(expected_custom_output,
                                    output_custom_feature)
@@ -294,10 +425,10 @@ class TestCustomFeature:
     def test_random_ts_and_power(self, df: pd.DataFrame, power: int):
         output_name = 'time_series'
 
-        custom_feature = CustomFeature(self.df_to_power, output_name,
+        custom_feature = CustomFeature(self._df_to_power, output_name,
                                        power=power)
 
         output_custom = custom_feature.fit_transform(df)
-        expected_custom_output = self.df_to_power(df, power)
+        expected_custom_output = self._df_to_power(df, power)
 
         testing.assert_frame_equal(expected_custom_output, output_custom)
