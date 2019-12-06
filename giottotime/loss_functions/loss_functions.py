fixed smape formula and seasonal feature

giottotime/loss_functions/loss_functions.py
@@ -28,7 +28,7 @@ def smape(y_true: Union[List, np.ndarray], y_pred: Union[List, np.ndarray]) -> f
         )
 
     non_normalized_smape = sum(
-        np.abs(y_pred - y_true) / (np.abs(y_pred) - np.abs(y_true))
+        np.abs(y_pred - y_true) / (np.abs(y_pred) + np.abs(y_true))
     )
     smape = (2 / len(y_pred)) * non_normalized_smape
     return smape