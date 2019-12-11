43fc93f670291eb9d5fe865a96b8f12a0f57a810
Stefano <stefano.savare@gmail.com>
2019-12-11 12:08:18 +0100

    Added splitters
    Signed-off-by: Stefano <stefano.savare@gmail.com>

--- giottotime/model_selection/train_test_splitters/base.py
+++ giottotime/model_selection/train_test_splitters/base.py
@@ -1,19 +1,26 @@
-from abc import ABCMeta, abstractmethod
+import pandas as pd
 
 
-class Splitter(metaclass=ABCMeta):
-    """ Base class for splitters. The children classes must implement
-    the transform method.
+class Splitter:
+    def __init__(
+        self, train_test_split_mode: str = "train_size", drop_na_mode: str = "any"
+    ):
+        self.train_test_split_mode = train_test_split_mode
+        self.drop_na_mode = drop_na_mode
 
-    Both the X and y matrices must be passed in the transform methods
-    """
+    def transform(
+        self, X: pd.DataFrame, y: pd.DataFrame, **kwargs
+    ) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame):
+        X, y = self.drop_na(X, y)
+        X_train, y_train, X_test, y_test = self.split_train_test(X, y)
+        return X_train, y_train, X_test, y_test
 
-    @abstractmethod
-    def transform(self, X, y, **kwargs):
+    def drop_na(self, X: pd.DataFrame, y: pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
+        X = X.dropna(axis=0, how=self.drop_na_mode)
+        y = y.loc[X.index]
+        return X, y
+
+    def split_train_test(
+        self, X: pd.DataFrame, y: pd.DataFrame
+    ) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame):
         pass
-
-    def fit(self, X, y=None, **kwargs):
-        return self
-
-    def fit_transform(self, X, y, **kwargs):
-        return self.transform(X, y, **kwargs)
