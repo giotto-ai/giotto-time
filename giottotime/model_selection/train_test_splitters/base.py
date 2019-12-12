48f21cd70383f3a292bbb45fe14d57573e19650b
Stefano <stefano.savare@gmail.com>
2019-12-12 10:22:19 +0100

    Added train test splitter
    Signed-off-by: Stefano <stefano.savare@gmail.com>

--- giottotime/model_selection/train_test_splitters/base.py
+++ giottotime/model_selection/train_test_splitters/base.py
@@ -1,21 +1,19 @@
+import numpy as np
 import pandas as pd
 
 
 class Splitter:
-    def __init__(
-        self, train_test_split_mode: str = "train_size", drop_na_mode: str = "any"
-    ):
-        self.train_test_split_mode = train_test_split_mode
+    def __init__(self, drop_na_mode: str = "any"):
         self.drop_na_mode = drop_na_mode
 
     def transform(
         self, X: pd.DataFrame, y: pd.DataFrame, **kwargs
     ) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame):
-        X, y = self.drop_na(X, y)
+        X, y = self.trim(X, y)
         X_train, y_train, X_test, y_test = self.split_train_test(X, y)
         return X_train, y_train, X_test, y_test
 
-    def drop_na(self, X: pd.DataFrame, y: pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
+    def trim(self, X: pd.DataFrame, y: pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
         X = X.dropna(axis=0, how=self.drop_na_mode)
         y = y.loc[X.index]
         return X, y
@@ -23,4 +21,9 @@ class Splitter:
     def split_train_test(
         self, X: pd.DataFrame, y: pd.DataFrame
     ) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame):
-        pass
+        y_nan = y.isnull().any(axis=1).replace(True, np.nan)
+        last_valid_index = y_nan.last_valid_index()
+        X_train, y_train = X.loc[:last_valid_index], y.loc[:last_valid_index]
+        test_indexes = y.index.difference(y_train.index)
+        X_test, y_test = X.loc[test_indexes], y.loc[test_indexes]
+        return X_train, y_train, X_test, y_test
