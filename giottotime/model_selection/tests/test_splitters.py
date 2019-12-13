d808774ceee3f07b39890cb48551e0645f047950
Stefano <stefano.savare@gmail.com>
2019-12-13 18:16:54 +0100

    Added tests for X_y_matrices:
    Signed-off-by: Stefano <stefano.savare@gmail.com>

--- giottotime/model_selection/tests/test_splitters.py
+++ giottotime/model_selection/tests/test_splitters.py
@@ -1,2 +1,35 @@
+import pytest
+
+import hypothesis.strategies as st
+from hypothesis import given
+
+from giottotime.feature_creation import ShiftFeature
+from giottotime.model_selection.feature_splitters import FeatureSplitter
+from giottotime.utils.hypothesis.feature_matrices import X_y_matrices
+
+
 class TestFeatureSplitter:
-    pass
+    def test_constructor(self):
+        FeatureSplitter()
+
+    @given(st.text().filter(lambda x: x != "any"))
+    def test_constructor_wrong_parameter(self, drop_na_mode: str):
+        with pytest.raises(ValueError):
+            FeatureSplitter(drop_na_mode)
+
+    @given(
+        X_y_matrices(
+            horizon=4,
+            time_series_features=[ShiftFeature(1, "1"), ShiftFeature(2, "2")],
+            allow_nan=False,
+        )
+    )
+    def test_transform(self, X_y):
+        X, y = X_y
+        feature_splitter = FeatureSplitter()
+        X_train, y_train, X_test, y_test = feature_splitter.transform(X, y)
+
+        assert X_train.shape == X.shape[0] - 2
+        assert y_train.shape == X.shape[0] - 2
+        assert X_test.shape == 2
+        assert y_test.shape == 2
--- giottotime/utils/hypothesis/feature_matrices.py
+++ giottotime/utils/hypothesis/feature_matrices.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 import hypothesis.strategies as st
 
@@ -8,7 +8,13 @@ from .time_indexes import giotto_time_series
 
 
 @st.composite
-def X_y_matrices(draw, horizon: int, time_series_features: List[Feature]):
+def X_y_matrices(
+    draw,
+    horizon: int,
+    time_series_features: List[Feature],
+    min_length: Optional[int] = None,
+    allow_nan_infinity: bool = True,
+):
     """ Returns a strategy that generates X and y feature matrices.
 
     Parameters
@@ -20,6 +26,12 @@ def X_y_matrices(draw, horizon: int, time_series_features: List[Feature]):
         the list of TimeSeriesFeature that is given as input to the
         FeaturesCreation
 
+    min_length : ``int``, optional, (default=``None``)
+        minimum length of the matrices
+
+    allow_nan_infinity : ``bool``, optional, (default=``True``)
+        allow nan and infinity in the starting time series
+
     Returns
     -------
     X : ``pd.DataFrame``
@@ -27,7 +39,14 @@ def X_y_matrices(draw, horizon: int, time_series_features: List[Feature]):
     y : ``pd.DataFrame``
         y feature matrix
     """
-    period_index_series = draw(giotto_time_series())
+    min_length = min_length if min_length is not None else 1
+    period_index_series = draw(
+        giotto_time_series(
+            min_length=min_length,
+            allow_nan=allow_nan_infinity,
+            allow_infinity=allow_nan_infinity,
+        )
+    )
     feature_creation = FeatureCreation(
         horizon=horizon, time_series_features=time_series_features
     )
@@ -36,7 +55,12 @@ def X_y_matrices(draw, horizon: int, time_series_features: List[Feature]):
 
 
 @st.composite
-def y_matrices(draw, horizon: int = 3):
+def y_matrices(
+    draw,
+    horizon: int = 3,
+    min_length: Optional[int] = None,
+    allow_nan_infinity: bool = True,
+):
     """ Returns a strategy that generates the y feature matrix.
 
     Parameters
@@ -44,17 +68,35 @@ def y_matrices(draw, horizon: int = 3):
     horizon : ``int``, optional, (default=3)
         the number of steps to forecast in the future. It affects the y shape.
 
+    min_length : ``int``, optional, (default=``None``)
+        minimum length of the matrices
+
+    allow_nan_infinity : ``bool``, optional, (default=``True``)
+        allow nan and infinity in the starting time series
+
     Returns
     -------
     y : ``pd.DataFrame``
             y feature matrix
     """
-    X, y = draw(X_y_matrices(horizon=horizon, time_series_features=[]))
+    X, y = draw(
+        X_y_matrices(
+            horizon=horizon,
+            time_series_features=[],
+            min_length=min_length,
+            allow_nan_infinity=allow_nan_infinity,
+        )
+    )
     return y
 
 
 @st.composite
-def X_matrices(draw, time_series_features: List[Feature]):
+def X_matrices(
+    draw,
+    time_series_features: List[Feature],
+    min_length: Optional[int] = None,
+    allow_nan_infinity: bool = True,
+):
     """ Returns a strategy that generates the X feature matrix.
 
     Parameters
@@ -63,10 +105,23 @@ def X_matrices(draw, time_series_features: List[Feature]):
         the list of TimeSeriesFeature that is given as input to the
         FeaturesCreation
 
+    min_length : ``int``, optional, (default=``None``)
+        minimum length of the matrices
+
+    allow_nan_infinity : ``bool``, optional, (default=``True``)
+        allow nan and infinity in the starting time series
+
     Returns
     -------
     X : ``pd.DataFrame``
             X feature matrix
     """
-    X, y = draw(X_y_matrices(horizon=1, time_series_features=time_series_features))
+    X, y = draw(
+        X_y_matrices(
+            horizon=1,
+            time_series_features=time_series_features,
+            min_length=min_length,
+            allow_nan_infinity=allow_nan_infinity,
+        )
+    )
     return X
