3b9ff44648f9e73ee2704cbb346948497f656abc
deatinor <stefano.savare@gmail.com>
2019-12-16 11:14:39 +0100

    Train test splitter (#70)
    * Added splitters
    
    Signed-off-by: Stefano <stefano.savare@gmail.com>
    
    * Added train test splitter
    
    Signed-off-by: Stefano <stefano.savare@gmail.com>
    
    * Refactored model selection
    
    Signed-off-by: Stefano <stefano.savare@gmail.com>
    
    * Added docs for train test splitters
    
    Signed-off-by: Stefano <stefano.savare@gmail.com>
    
    * Added tests for X_y_matrices:
    
    Signed-off-by: Stefano <stefano.savare@gmail.com>
    
    * Added tests for train_test
    
    Signed-off-by: Stefano <stefano.savare@gmail.com>
    
    * Fix test
    
    Signed-off-by: Stefano <stefano.savare@gmail.com>
    
    * Removed defines_strategy
    
    Signed-off-by: Stefano <stefano.savare@gmail.com>
    
    * Fixed hypothesis version bug
    
    Signed-off-by: Stefano <stefano.savare@gmail.com>
    
    * Removed experimental in model selection
    
    Signed-off-by: Stefano <stefano.savare@gmail.com>

---
+++ giottotime/utils/hypothesis/tests/test_feature_matrices.py
@@ -0,0 +1,98 @@
+from typing import Tuple
+
+import pandas as pd
+from hypothesis import given
+
+from giottotime.feature_creation import ShiftFeature, MovingAverageFeature
+from giottotime.utils.hypothesis.feature_matrices import (
+    X_y_matrices,
+    X_matrices,
+    y_matrices,
+)
+
+features = [
+    ShiftFeature(1, "1"),
+    ShiftFeature(2, "2"),
+    MovingAverageFeature(3, "3"),
+]
+
+
+class TestXyMatrices:
+    @given(X_y_matrices(horizon=1, time_series_features=features))
+    def test_horizon_1(self, X_y: Tuple[pd.DataFrame, pd.DataFrame]):
+        X, y = X_y
+        assert y.shape[1] == 1
+
+    @given(X_y_matrices(horizon=2, time_series_features=features))
+    def test_horizon_2(self, X_y: Tuple[pd.DataFrame, pd.DataFrame]):
+        X, y = X_y
+        assert y.shape[1] == 2
+
+    @given(X_y_matrices(horizon=3, time_series_features=features))
+    def test_horizon_3(self, X_y: Tuple[pd.DataFrame, pd.DataFrame]):
+        X, y = X_y
+        assert y.shape[1] == 3
+
+    @given(X_y_matrices(horizon=3, time_series_features=features))
+    def test_X_column_names(self, X_y: Tuple[pd.DataFrame, pd.DataFrame]):
+        X, y = X_y
+        assert list(X.columns) == ["1", "2", "3"]
+
+    @given(X_y_matrices(horizon=3, time_series_features=features))
+    def test_X_shape_correct(self, X_y: Tuple[pd.DataFrame, pd.DataFrame]):
+        X, y = X_y
+        assert X.shape[1] == len(features)
+
+    @given(X_y_matrices(horizon=3, time_series_features=features))
+    def test_y_column_names(self, X_y: Tuple[pd.DataFrame, pd.DataFrame]):
+        X, y = X_y
+        assert list(y.columns) == ["y_0", "y_1", "y_2"]
+
+    @given(X_y_matrices(horizon=3, time_series_features=features))
+    def test_shape_consistent(self, X_y: Tuple[pd.DataFrame, pd.DataFrame]):
+        X, y = X_y
+        assert X.shape[0] == y.shape[0]
+
+    @given(
+        X_y_matrices(horizon=3, time_series_features=features, allow_nan_infinity=False)
+    )
+    def test_allow_nan_false(self, X_y: Tuple[pd.DataFrame, pd.DataFrame]):
+        X, y = X_y
+        assert X.dropna(axis=0, how="any").shape[0] == max(X.shape[0] - 3, 0)
+        assert y.dropna(axis=0, how="any").shape[0] == max(y.shape[0] - 2, 0)
+
+
+class TestXMatrices:
+    @given(X_matrices(time_series_features=features))
+    def test_X_shape_correct(self, X: pd.DataFrame):
+        assert X.shape[1] == len(features)
+
+    @given(X_matrices(time_series_features=features))
+    def test_X_column_names(self, X: pd.DataFrame):
+        assert list(X.columns) == ["1", "2", "3"]
+
+    @given(X_matrices(time_series_features=features, allow_nan_infinity=False))
+    def test_allow_nan_false(self, X: pd.DataFrame):
+        assert X.dropna(axis=0, how="any").shape[0] == max(X.shape[0] - 3, 0)
+
+
+class TestYMatrices:
+    @given(y_matrices(horizon=1))
+    def test_horizon_1(self, y: pd.DataFrame):
+        assert y.shape[1] == 1
+
+    @given(y_matrices(horizon=2))
+    def test_horizon_2(self, y: pd.DataFrame):
+        assert y.shape[1] == 2
+
+    @given(y_matrices(horizon=3))
+    def test_horizon_3(self, y: pd.DataFrame):
+        assert y.shape[1] == 3
+
+    @given(y_matrices(horizon=3))
+    def test_y_column_names(self, y: pd.DataFrame):
+        assert list(y.columns) == ["y_0", "y_1", "y_2"]
+
+    @given(y_matrices(horizon=3, allow_nan_infinity=False))
+    def test_allow_nan_false(self, y: pd.DataFrame):
+        assert y.dropna(axis=0, how="any").shape[0] == max(y.shape[0] - 2, 0)
