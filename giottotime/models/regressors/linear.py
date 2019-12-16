4aa5e063dc6709cfe231ff106829b136029acb49
Benjamin Russell <benjaminrussell@philippes-mbp.lan>
2019-12-16 10:43:16 +0100

    removed r2_seed from linear regressor

--- giottotime/models/regressors/linear.py
+++ giottotime/models/regressors/linear.py
@@ -25,7 +25,7 @@ class LinearRegressor:
     ):  # weight_initialization_rule = lambda X, y: np.zeros(X.shape[1]) ):
         self.loss = loss
 
-    def fit(self, X: pd.DataFrame, y: pd.DataFrame, disp: bool = False, **kwargs):
+    def fit(self, X: pd.DataFrame, y: pd.DataFrame, **kwargs):
         """Fit the linear model on ``X`` and ``y`` on the given loss function.
         To do the minimization, the ``scipy.optimize.minimize`` function is
         used. To have more details and check which kind of options are
@@ -58,19 +58,6 @@ class LinearRegressor:
                 model_weights[0] + np.dot(model_weights[1:], row) for row in X.values
             ]
             return self.loss(y, predictions)
-
-        if "r2_seed" in kwargs:
-            lm = LinearRegression(fit_intercept=True).fit(X, y)
-            self.r2_seed = [lm.intercept_] + list(lm.coef_)
-            kwargs["x0"] = self.r2_seed
-            del kwargs["r2_seed"]
-
-        if "x0" not in kwargs:
-            kwargs["x0"] = np.zeros(
-                X.shape[1] + 1
-            )  # weight_initialization_rule(X, y) np.zeros(X.shape[1]+1)
-        else:
-            kwargs["x0"] = kwargs["x0"] + [0] * (X.shape[1] + 1 - len(kwargs["x0"]))
 
         res = minimize(prediction_error, options={"disp": disp}, **kwargs)
 
