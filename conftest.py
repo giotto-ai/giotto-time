bc9ac81cb5cef8c141e312e7a4ec3b84ea8c011e
Stefano <stefano.savare@gmail.com>
2019-12-16 14:54:57 +0100

    Removed flake8 check
    Signed-off-by: Stefano <stefano.savare@gmail.com>

--- conftest.py
+++ conftest.py
@@ -1,5 +1,17 @@
-from hypothesis import settings, Verbosity
+from datetime import timedelta
 
-settings.register_profile("ci", max_examples=100)
-settings.register_profile("dev", max_examples=7)
+from hypothesis import settings, Verbosity, HealthCheck
+
+settings.register_profile(
+    "ci",
+    max_examples=100,
+    suppress_health_check=(HealthCheck.too_slow,),
+    deadline=timedelta(milliseconds=500),
+)
+settings.register_profile(
+    "dev",
+    max_examples=7,
+    suppress_health_check=(HealthCheck.too_slow,),
+    deadline=timedelta(milliseconds=500),
+)
 settings.register_profile("debug", max_examples=7, verbosity=Verbosity.verbose)
