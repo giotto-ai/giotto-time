f9947fb3855a3efd4f0f0f07c445783bb21825b3
Stefano <stefano.savare@gmail.com>
2019-12-16 17:45:47 +0100

    Refactored test jobs
    Signed-off-by: Stefano <stefano.savare@gmail.com>

--- conftest.py
+++ conftest.py
@@ -6,12 +6,12 @@ settings.register_profile(
     "ci",
     max_examples=100,
     suppress_health_check=(HealthCheck.too_slow,),
-    deadline=timedelta(milliseconds=500),
+    deadline=timedelta(milliseconds=1000),
 )
 settings.register_profile(
     "dev",
     max_examples=7,
     suppress_health_check=(HealthCheck.too_slow,),
-    deadline=timedelta(milliseconds=500),
+    deadline=timedelta(milliseconds=1000),
 )
 settings.register_profile("debug", max_examples=7, verbosity=Verbosity.verbose)
