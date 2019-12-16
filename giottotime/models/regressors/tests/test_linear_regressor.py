ee318a6831c6dead499c15f9289a7af166732d59
Benjamin Russell <benjaminrussell@philippes-mbp.lan>
2019-12-16 14:37:29 +0100

    added additional tests to linear regressor

--- giottotime/models/regressors/tests/test_linear_regressor.py
+++ giottotime/models/regressors/tests/test_linear_regressor.py
@@ -4,6 +4,9 @@ import pandas as pd
 from hypothesis import given, strategies as st
 
 from pandas.util import testing as testing
+from hypothesis.extra.numpy import arrays
+from hypothesis.strategies import floats
+from hypothesis import given, settings
 
 from giottotime.models.regressors.linear_regressor import LinearRegressor
 
@@ -11,30 +14,61 @@ from random import random
 
 class TestLinearRegressor:
     def test_linear_regressor_linear_regressor(self):
-        a, b = random()*10, 2*(1-random())
+        a1, a2, b = random()*10, random()*100, 2*(1-random())
 
         testing.N, testing.K = 100, 1
         df = pd.DataFrame()
 
         df['x1'] = list(range(100))
-        df['y'] = [ a + b*t for t in range(100) ]
+        df['x2'] = [random() for k in range(100)]
+
+        df['y'] = [ b + a1*t for t in range(100) ]
+        df['y'] = df['y'] + a2*df['x2']
 
         train = df[:90]
         test = df[90:]
 
         lr = LinearRegressor()
 
-        lr.fit(train[['x1']], train['y'], x0=[0, 0])
+        lr.fit(train[['x1', 'x2']], train['y'], x0=[0, 0, 0])
 
-        preds_y = lr.predict(test[['x1']])
+        preds_y = lr.predict(test[['x1', 'x2']])
         test_y = test['y'].values
-
-        #print(preds_y)
-        #print(test_y)
 
         np.testing.assert_array_almost_equal(preds_y, test_y, decimal=2)
 
-        return False
+    @settings(deadline=None)
+    @given(arrays(dtype=float, shape=(100, 1), elements=floats(-10000000, 10000000)))
+    def test_linear_regressor_linear_regressor_random_array(self, random_array):
+        a1, a2, b = random()*10, random()*100, 2*(1-random())
+
+        testing.N, testing.K = 100, 1
+        df = pd.DataFrame()
+
+        df['x1'] = list(range(100))
+        df['x2'] = random_array
+
+        df['y'] = [ b + a1*t for t in range(100) ]
+        df['y'] = df['y'] + a2*df['x2']
+
+        train = df[:90]
+        test = df[90:]
+
+        lr = LinearRegressor()
+
+        lr.fit(train[['x1', 'x2']], train['y'], x0=[0, 0, 0])
+
+        preds_y = lr.predict(test[['x1', 'x2']])
+        test_y = test['y'].values
+
+        preds_y = preds_y/np.sum(preds_y)
+        test_y = test_y/np.sum(test_y)
+
+        print(preds_y)
+        print(test_y)
+        print("\n\n±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±\n\n")
+
+        np.testing.assert_array_almost_equal(preds_y, test_y, decimal=0)
 
 
 #
