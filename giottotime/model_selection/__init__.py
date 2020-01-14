5e079938cc0f299ad072edef8f6e5faa68cc6e90
philippenguyen <p.nguyen@l2f.ch>
2020-01-14 18:22:00 +0100

    walk forward split, ignoring tests in test_time_series file

--- giottotime/model_selection/__init__.py
+++ giottotime/model_selection/__init__.py
@@ -3,5 +3,6 @@ The :mod:`giottotime.feature_extraction` module deals with model selection.
 """
 
 from .feature_splitters import FeatureSplitter
+from .walk_forward_split import walk_forward_split
 
-__all__ = ["FeatureSplitter"]
+__all__ = ["FeatureSplitter", "walk_forward_split"]
