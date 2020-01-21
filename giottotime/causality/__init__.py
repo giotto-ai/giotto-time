58163106fee762d93179606bcbe4e79636373d84
philippenguyen <p.nguyen@l2f.ch>
2020-01-21 17:15:24 +0100

    make causality test transformers, renaming

--- giottotime/causality/__init__.py
+++ giottotime/causality/__init__.py
@@ -1,15 +1,13 @@
 """
-The :mod:`giottotime.causality_tests` module deals with the causality tests for time
+The :mod:`giottotime.causality` module deals with the causality tests for time
 series data.
 """
 
-from .base import CausalityTest
 from .shifted_linear_coefficient import ShiftedLinearCoefficient
 from .shifted_pearson_correlation import ShiftedPearsonCorrelation
 
 
 __all__ = [
-    "CausalityTest",
     "ShiftedLinearCoefficient",
     "ShiftedPearsonCorrelation",
 ]
--- giottotime/causality/tests/test_causality_tests.py
+++ giottotime/causality/tests/test_causality_tests.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 from pandas.util import testing as testing
 
-from giottotime.causality_tests import (
+from giottotime.causality import (
     ShiftedLinearCoefficient,
     ShiftedPearsonCorrelation,
 )
