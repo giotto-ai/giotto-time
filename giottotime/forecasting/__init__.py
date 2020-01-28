54760361907115400b17977873ee79215614ae23
philippenguyen <p.nguyen@l2f.ch>
2020-01-28 13:40:24 +0100

    relative import

--- giottotime/forecasting/__init__.py
+++ giottotime/forecasting/__init__.py
@@ -3,7 +3,7 @@ The :mod:`giottotime.feature_extraction` module contains a collection of differe
 of machine learning models, for dealing with time series and non time series data.
 """
 
-from giottotime.regressors import LinearRegressor
+from ..regressors import LinearRegressor
 from .gar import GAR, GARFF
 from .trend_models import TrendForecaster
 
