d99ef43acb582ef42fc3c65cada7d4cecc10c213
alexbacce <alessio.baccelli95@gmail.com>
2020-02-05 10:13:37 +0100

    Fixed p-value bug #129

--- gtime/causality/base.py
+++ gtime/causality/base.py
@@ -84,7 +84,7 @@ class CausalityMixin:
 
     def _compute_is_test_significant(self, data, x, y, best_shift):
         bootstrap_matrix = data.copy()
-        bootstrap_matrix[y] = bootstrap_matrix.shift(best_shift)[y]
+        bootstrap_matrix[x] = bootstrap_matrix.shift(best_shift)[x]
         bootstrap_matrix.dropna(axis=0, inplace=True)
         rhos = []
 
@@ -92,7 +92,6 @@ class CausalityMixin:
             bootstraps = bootstrap_matrix.sample(n=len(data), replace=True)
             rhos.append(stats.pearsonr(bootstraps[x], bootstraps[y])[0])
         rhos = pd.DataFrame(rhos)
-
         percentile = stats.percentileofscore(rhos, 0) / 100
         p_value = 2 * (percentile if percentile < 0.5 else 1 - percentile)
 
