081c524346bd3e2c4f662ba808bea82a2eb4b259
philippenguyen <p.nguyen@l2f.ch>
2020-02-07 11:40:55 +0100

    Test for FeatureCreation transform

--- gtime/compose/tests/test_feature_creation.py
+++ gtime/compose/tests/test_feature_creation.py
@@ -1,1 +1,16 @@
-# TODO: tests
+import pandas.util.testing as testing
+from numpy.testing import assert_array_equal
+
+from gtime.compose import FeatureCreation
+from gtime.feature_extraction import Shift, MovingAverage
+
+
+def test_feature_creation_transform():
+    data = testing.makeTimeDataFrame(freq="s")
+    fc = FeatureCreation([
+        ('s1', Shift(1), ['A']),
+        ('ma3', MovingAverage(window_size=3), ['B']),
+    ])
+    res = fc.fit(data).transform(data)
+
+    assert_array_equal(res.columns, fc.fit_transform(data).columns.values)
