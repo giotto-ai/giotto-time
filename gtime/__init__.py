97e7bc8770b692937ec3af98f3f75757b270015c
philippenguyen <p.nguyen@l2f.ch>
2020-02-24 17:08:06 +0100

    update doc

--- gtime/__init__.py
+++ gtime/__init__.py
@@ -1,19 +1,15 @@
-"""
-Time series module for Python
-==================================
-giotto-time is a Python module integrating time series and machine learning.
-
-See URL TO GIOTTOTIME for complete documentation.
-"""
-
 from gtime._version import __version__
-
 
 __all__ = [
     "causality",
+    "compose",
     "feature_extraction",
+    "feature_generation",
+    "forecasting",
     "metrics",
     "model_selection",
-    "forecasting",
     "preprocessing",
+    "regressors",
+    "utils",
 ]
+
