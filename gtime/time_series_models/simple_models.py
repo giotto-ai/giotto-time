95d0bc198442caca420896f3a111c88dfa3c77a2
Simon B <sburyachenko@gmail.com>
2020-02-26 17:49:19 -0500

    Pipelines update

--- gtime/time_series_models/simple_models.py
+++ gtime/time_series_models/simple_models.py
@@ -1,10 +1,9 @@
 import pandas as pd
 from gtime.compose import FeatureCreation
 from sklearn.compose import make_column_selector
-from gtime.model_selection import horizon_shift, FeatureSplitter
 from gtime.feature_extraction import Shift, MovingAverage, MovingCustomFunction
 from gtime.time_series_models import TimeSeriesForecastingModel
-from gtime.forecasting import NaiveModel, SeasonalNaiveModel, DriftModel
+from gtime.forecasting import NaiveModel, SeasonalNaiveModel, DriftModel, AverageModel
 
 
 class NaiveForecastModel(TimeSeriesForecastingModel):
@@ -15,22 +14,24 @@ class NaiveForecastModel(TimeSeriesForecastingModel):
             ])
         super().__init__(features=features, horizon=horizon, model=NaiveModel())
 
+
 class AverageForecastModel(TimeSeriesForecastingModel):
 
-    def __init__(self, horizon: int, window_size: int):
+    def __init__(self, horizon: int):
         features = FeatureCreation(
-            [('ma_' + str(window_size), MovingAverage(window_size=window_size), make_column_selector()),
+            [('s1', Shift(0), make_column_selector()),
             ])
-        super().__init__(features=features, horizon=horizon, model=NaiveModel())
+        super().__init__(features=features, horizon=horizon, model=AverageModel())
+
 
 class SeasonalNaiveForecastModel(TimeSeriesForecastingModel):
 
-
-    def __init__(self, horizon: int, seasonal_length: int = 1):
+    def __init__(self, horizon: int, seasonal_length: pd.Timedelta):
         features = FeatureCreation(
             [('s1', Shift(0), make_column_selector()),
             ])
         super().__init__(features=features, horizon=horizon, model=SeasonalNaiveModel(seasonal_length))
+
 
 class DriftForecastModel(TimeSeriesForecastingModel):
 
@@ -39,6 +40,3 @@ class DriftForecastModel(TimeSeriesForecastingModel):
             [('s1', Shift(0), make_column_selector()),
             ])
         super().__init__(features=features, horizon=horizon, model=DriftModel())
-
-
-
