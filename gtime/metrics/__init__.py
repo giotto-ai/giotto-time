9cb2cb7bb2be7f673c7e58996f707bfcc366d72d
SrinidhiPatil <sripatil1994@gmail.com>
2020-02-27 21:25:15 +0530

    Updated test File

--- gtime/metrics/__init__.py
+++ gtime/metrics/__init__.py
@@ -2,9 +2,12 @@
 The :mod:`gtime.metrics` module contains a collection of different metrics.
 """
 
-from .metrics import smape, max_error
+from .metrics import smape, max_error, mse, log_mse, r_square
 
 __all__ = [
     "smape",
     "max_error",
+    "mse",
+    "log_mse",
+    "r_square",
 ]
