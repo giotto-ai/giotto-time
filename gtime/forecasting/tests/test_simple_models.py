992811f879e90abb30a686c5022f5f803aca5850
Simon B <sburyachenko@gmail.com>
2020-03-03 20:23:36 -0400

    Seasonal naive models tests

--- gtime/forecasting/tests/test_simple_models.py
+++ gtime/forecasting/tests/test_simple_models.py
@@ -38,15 +38,37 @@ class TestNaiveModel:
         self.model = NaiveModel()
         self.model.fit(X_train, y_train)
         self.X_test = X_test
+        self.y_pred = self.model.predict(X_test)
 
-    def test_fit(self):
+    def test_fit_horizon(self):
         assert self.model._horizon == len(self.X_test)
 
-    def test_predict(self):
+    def test_predict_shape(self):
+        assert self.y_pred.shape == (self.model._horizon, self.model._horizon)
+
+    def test_predict_df(self):
         horizon = len(self.X_test)
-        y_pred = self.model.predict(self.X_test)
         y_cols = ['y_'+str(x+1) for x in range(len(self.X_test))]
         res = np.broadcast_to(self.X_test, (horizon, horizon))
         expected_df = pd.DataFrame(res, index=self.X_test.index, columns=y_cols)
-        testing.assert_frame_equal(y_pred, expected_df)
+        testing.assert_frame_equal(self.y_pred, expected_df)
 
+
+class TestSeasonalNaiveModel(TestNaiveModel):
+
+    @given(data=forecast_input(50), season_length=st.integers(min_value=1, max_value=100))
+    def setup(self, data, season_length):
+        X_train, y_train, X_test = data
+        self.season_length = season_length
+        self.model = SeasonalNaiveModel(season_length)
+        self.model.fit(X_train, y_train)
+        self.X_test = X_test
+        note(season_length)
+        self.y_pred = self.model.predict(X_test)
+
+    def test_predict_seasonality(self):
+        if self.season_length < self.model._horizon:
+            assert all(self.y_pred.iloc[:, 0] == self.y_pred.iloc[:, self.season_length])
+
+    def test_predict_df(self):
+        pass
