97e734651b91025aaf477f60136c1ed7165adaac
Simon B <sburyachenko@gmail.com>
2020-03-28 17:58:05 -0300

    Week of the year issue fixed

--- gtime/plotting/preprocessing.py
+++ gtime/plotting/preprocessing.py
@@ -19,6 +19,16 @@ def seasonal_split(df: pd.DataFrame, cycle: str = 'year', freq=None, agg='mean')
     pd.DataFrame with seasonal columns
 
     """
+    def _week_of_year(t: pd.Period):
+        # pandas weekofyear inconsistency fix, according to ISO week date
+        if t.start_time.weekofyear == 1 and t.start_time.month == 12:
+            year = t.end_time.year
+            week = t.start_time.weekofyear
+        else:
+            year = t.start_time.year
+            week = t.start_time.weekofyear
+        return '_'.join([str(year), str(week)])
+
     if freq is None:
         freq = df.index.freqstr
     df = df.resample(freq).agg(agg)
@@ -29,38 +39,40 @@ def seasonal_split(df: pd.DataFrame, cycle: str = 'year', freq=None, agg='mean')
             df['_Series'] = df.index.start_time.year
             if freq == 'D':
                 df['_Season'] = df.index.dayofyear
+            elif freq in ['W', 'W-SUN']:
+                df['_Season'] = df.start_time.weekofyear
             elif freq == 'M':
-                df['_Season'] = df.index.month
+                df['_Season'] = df.index.start_time.month
             elif freq in ['Q', 'Q-DEC']:
-                df['_Season'] = df.index.quarter
+                df['_Season'] = df.index.start_time.quarter
             else:
                 df['_Season'] = df[col_name].groupby(pd.Grouper(freq="Y", convention='s')).cumcount() + 1
 
         elif cycle == 'quarter':
-            df['_Series'] = list(map(lambda x: '_'.join([str(x.year), str(x.quarter)]), df.index))
+            df['_Series'] = list(map(lambda x: '_'.join([str(x.start_time.year), str(x.start_time.quarter)]), df.index))
             df['_Season'] = df[col_name].groupby(pd.Grouper(freq="Q", convention='s')).cumcount() + 1
 
         elif cycle == 'month':
-            df['_Series'] = list(map(lambda x: '_'.join([str(x.year), str(x.month)]), df.index))
+            df['_Series'] = list(map(lambda x: '_'.join([str(x.start_time.year), str(x.start_time.month)]), df.index))
             if freq == 'D':
                 df['_Season'] = df.index.day
             else:
                 df['_Season'] = df[col_name].groupby(pd.Grouper(freq="M", convention='s')).cumcount() + 1
 
         elif cycle == 'week':
-            df['_Series'] = list(map(lambda x: '_'.join([str(x.year), str(x.weekofyear)]), df.index))
+            df['_Series'] = list(map(_week_of_year, df.index))
             if freq == 'D':
-                df['_Season'] = df.index.day
+                df['_Season'] = df.index.dayofweek
             else:
                 df['_Season'] = df[col_name].groupby(pd.Grouper(freq="W", convention='s')).cumcount() + 1
         else:
             raise ValueError("Incorrect cycle period name")
     else:
-        df['_Series'] = df.resample(cycle)
-        s = []
-        for i, j in df.resample(freq):
-            s += [i.__str__()] * len(j)
-        df['_Season'] = s
+        df['_Series'] = df[col_name].groupby(pd.Grouper(freq=cycle, convention='s')).ngroup()
+        df['_Season'] = df[col_name].groupby(pd.Grouper(freq=cycle, convention='s')).cumcount() + 1
+
+    if any(df.set_index(['_Series', '_Season']).index.duplicated()):
+        print('BUG!!!')
 
     return df.set_index(['_Series', '_Season']).unstack(level=0)
 
