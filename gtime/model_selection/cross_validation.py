c6e69ecbbf9bb9e75f8fe63b7d32f6cadaf4464e
SrinidhiPatil <sripatil1994@gmail.com>
2020-04-02 22:13:59 +0530

    Tests for Time Series Split

--- gtime/model_selection/cross_validation.py
+++ gtime/model_selection/cross_validation.py
@@ -1,10 +1,7 @@
-import numpy as np
 import pandas as pd
-import math
-from datetime import date, datetime, timedelta
 
 
-def time_series_split(time_series, n_splits=4, split_on='index'):
+def time_series_split(time_series: pd.DataFrame, n_splits=4, split_on='index'):
    """
    Split the input DataFrame into n_splits. If the data is not a timeries then the split
    is based on the number of samples.
@@ -14,27 +11,50 @@ def time_series_split(time_series, n_splits=4, split_on='index'):
    based on time
 
    Parameters
-    ----------
-    time_series : pandas DataFrame, shape (n_samples,), required
-    The dataframe should have datetime as index if it is a timeseries data
+   ----------
+   time_series : pandas DataFrame, shape (n_samples,), required
+   The dataframe should have datetime as index if it is a timeseries data
 
-    n_splits : int, default = 4, required
-    The number of splits/folds on the dataset
+   n_splits : int, default = 4, required
+   The number of splits/folds on the dataset
 
-    split_on : 'index', default parameter
-    If the index is a datetime then the dataset will be split based on time
+   split_on : 'index', default parameter
+   If the index is a datetime then the dataset will be split based on time
 
-    Yields
-    -------
-    fold.index : list/lists of pandas indexes of folds
-    time_fold.index : list/lists of pandas indexes of folds
+   Yields
+   -------
+   fold.index : list/lists of pandas indexes of folds
+   time_fold.index : list/lists of pandas indexes of folds
 
    Examples
    --------
-   
+   Example 1)
+   >>> date_rng = pd.date_range(start='1/1/2018', end='1/08/2018', freq='D')
+   >>> time_series = pd.DataFrame(date_rng, columns=['date'])
+   >>> time_series.set_index('date', inplace=True)
+   >>> time_series['data'] = np.random.randint(0,100,size=(len(date_rng)))
+   >>> for fold in (time_series_split(time_series, n_splits=4, split_on='time')):
+   ...      print(fold)
+   DatetimeIndex(['2018-01-01', '2018-01-02'], dtype='datetime64[ns]', name='date', freq=None)
+   DatetimeIndex(['2018-01-01', '2018-01-02', '2018-01-03', '2018-01-04'], 
+                  dtype='datetime64[ns]', name='date', freq=None)
+   DatetimeIndex(['2018-01-01', '2018-01-02', '2018-01-03', '2018-01-04',
+                  '2018-01-05', '2018-01-06'],
+                   dtype='datetime64[ns]', name='date', freq=None)
+   DatetimeIndex(['2018-01-01', '2018-01-02', '2018-01-03', '2018-01-04',
+                  '2018-01-05', '2018-01-06', '2018-01-07', '2018-01-08'],
+                   dtype='datetime64[ns]', name='date', freq=None)
+
+   Example 2)
+   >>> df = pd.DataFrame(np.random.randint(0,100,size=(16, 4)), columns=list('ABCD'))
+   >>> for fold in (time_series_split(df, n_splits=4, split_on='index')):
+   ...      print(fold)
+   RangeIndex(start=0, stop=4, step=1)
+   RangeIndex(start=0, stop=8, step=1)
+   RangeIndex(start=0, stop=12, step=1)
+   RangeIndex(start=0, stop=16, step=1)
+
    """
-   # TODO : Write Tests
-   # TODO : Examples in Doctrings
    
    n_samples = len(time_series)
    if n_samples < n_splits:
@@ -51,20 +71,22 @@ def time_series_split(time_series, n_splits=4, split_on='index'):
          yield fold.index
          end += n_set
       last_fold = time_series[start:]
+      yield last_fold.index
 
    elif split_on == 'time':
       if isinstance(time_series.index, pd.DatetimeIndex):
          start_date = time_series.index[0] 
          end_date = time_series.index[-1]
-         n_days = len(time_series.resample('D'))
-         fold_length = n_days // n_splits
-         next_date = start_date + pd.Timedelta(days=fold_length)
+         split_length = ((end_date - start_date) / n_splits)
+         next_date = start_date + pd.Timedelta(split_length)
 
          for split in range(n_splits - 1):
             time_fold = time_series[(time_series.index >= start_date) & (time_series.index < next_date)]
             yield time_fold.index
-            next_date += pd.Timedelta(days=fold_length)
+            next_date += pd.Timedelta(split_length)
          last_time_fold = time_series[0:]
+         yield last_time_fold.index
+
       else:
          raise ValueError(
             "The input parameter split_on is 'time' but the data does not have time index"
---
+++ gtime/model_selection/tests/test_cross_validation.py
@@ -0,0 +1,80 @@
+import pandas as pd 
+import numpy as np
+import pytest 
+from gtime.model_selection import time_series_split
+
+
+class TestTimeSeriesSplit:
+    def _correct_split_on_time_record_length(self):
+        date_rng = pd.date_range(start='1/1/2018', end='1/08/2018', freq='D')
+        time_series = pd.DataFrame(date_rng, columns=['date'])
+        time_series.set_index('date', inplace=True)
+        time_series['data'] = np.random.randint(0,100,size=(len(date_rng)))
+        n_splits = 5
+        start_date = time_series.index[0] 
+        end_date = time_series.index[-1]
+        split_length = ((end_date - start_date) / n_splits)
+        next_date = start_date + pd.Timedelta(split_length)
+
+        for split in range(n_splits - 1):
+            time_fold = time_series[(time_series.index >= start_date) & (time_series.index < next_date)]
+            yield time_fold.index
+            next_date += pd.Timedelta(split_length)
+        last_time_fold = time_series[0:]
+        yield last_time_fold.index
+
+    def test_samples_split_counts(self):
+        date_rng = pd.date_range(start='1/1/2018', end='1/08/2018', freq='D')
+        time_series = pd.DataFrame(date_rng, columns=['date'])
+        time_series.set_index('date', inplace=True)
+        time_series['data'] = np.random.randint(0,100,size=(len(date_rng)))
+
+        with pytest.raises(ValueError):
+            for element in (time_series_split(time_series, n_splits=(len(time_series)+1), split_on='index')):
+                pass
+
+    def check_split_on_time_index_type(self):
+        # If the split_on is set to 'time' but index is not DateTime
+        date_rng = pd.date_range(start='1/1/2018', end='1/08/2018', freq='D')
+        time_series = pd.DataFrame(date_rng, columns=['date'])
+        time_series['data'] = np.random.randint(0,100,size=(len(date_rng)))
+
+        with pytest.raises(ValueError):
+            for element in (time_series_split(time_series, n_splits=5, split_on='time')):
+                pass
+
+    def split_on_index(self):
+        date_rng = pd.date_range(start='1/1/2018', end='1/08/2018', freq='D')
+        time_series = pd.DataFrame(date_rng, columns=['date'])
+        time_series.set_index('date', inplace=True)
+        time_series['data'] = np.random.randint(0,100,size=(len(date_rng)))
+
+        splits = 5
+        split_length = len(time_series) // splits
+        fold_length = split_length
+        length_list = []
+        
+        for fold_index in time_series_split(time_series, n_splits=splits, split_on='index'):
+            length_list.append(len(fold_index))
+        for index_length in range(len(length_list)-1):
+            assert fold_length == length_list[index_length]
+            fold_length += split_length 
+        last_fold_length = len(time_series)
+        
+        assert last_fold_length == length_list[-1]
+
+    def split_on_time(self):
+        date_rng = pd.date_range(start='1/1/2018', end='1/08/2018', freq='D')
+        time_series = pd.DataFrame(date_rng, columns=['date'])
+        time_series.set_index('date', inplace=True)
+        time_series['data'] = np.random.randint(0,100,size=(len(date_rng)))
+
+        record_count = []
+        for fold_index in time_series_split(time_series, n_splits=5, split_on='time'):
+            record_count.append(len(fold_index))
+        correct_record_count = []
+        for fold_index in self._correct_split_on_time_record_length():
+            correct_record_count.append(len(fold_index))
+        
+        assert all([a == b for a, b in zip(correct_record_count, record_count)])
+    
