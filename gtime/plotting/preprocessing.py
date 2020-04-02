0eb625c0db01556c9d67e32a3cc79d6bd5461c59
Simon B <sburyachenko@gmail.com>
2020-04-02 17:27:12 -0300

    #162 Code refactoring

--- gtime/plotting/preprocessing.py
+++ gtime/plotting/preprocessing.py
@@ -1,9 +1,110 @@
 import pandas as pd
 import numpy as np
+from typing import Optional, Union, Callable
 from scipy.linalg import toeplitz
 
 
-def seasonal_split(df: pd.DataFrame, cycle: str = "year", freq=None, agg="mean"):
+def _week_of_year(t: pd.Period) -> str:
+
+    """
+    Week of the year, according to ISO week date format, returned as `year_week`
+
+    Parameters
+    ----------
+    t : pd.Period, input period
+
+    Returns
+    -------
+    str, week of the year
+
+    """
+
+    if t.start_time.weekofyear == 1 and t.start_time.month == 12:
+        year = t.end_time.year
+        week = t.start_time.weekofyear
+    elif t.start_time.weekofyear == 52 and t.end_time.month == 1:
+        year = t.end_time.year - 1
+        week = t.start_time.weekofyear
+    else:
+        year = t.start_time.year
+        week = t.start_time.weekofyear
+
+    return "_".join([str(year), str(week)])
+
+
+def _get_season_names(df: pd.DataFrame, cycle: str, freq: Optional[str] = None) -> pd.DataFrame:
+
+    """
+    Gets names of each season (days of week, months of year etc.) of ``df`` period index
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+    cycle : str, cycle, calendar term ('year', 'quarter', 'month', 'week') or pandas offset string
+    freq : str, series frequency to resample to
+
+    Returns
+    -------
+    pd.DataFrame, a column of season names
+
+    """
+
+    calendar_map = {
+        ('year', 'D'): 'index.start_time.dayofyear',
+        ('year', 'W'): 'index.start_time.weekofyear',
+        ('year', 'W-SUN'): 'index.start_time.weekofyear',
+        ('year', 'M'): 'index.start_time.dayofyear',
+        ('year', 'Q'): 'index.start_time.quarter',
+        ('year', 'Q-DEC'): 'index.start_time.quarter',
+        ('month', 'D'): 'index.start_time.day',
+        ('week', 'D'): 'index.start_time.dayofweek',
+    }
+
+    freq_map = {
+        'year': 'Y',
+        'quarter': 'Q',
+        'month': 'M',
+        'week': 'W'
+    }
+
+    if (cycle, freq) in calendar_map.keys():
+        return getattr(df, calendar_map[(cycle, freq)])
+    else:
+        col_name = df.columns[0]
+        if cycle in freq_map.keys():
+            cycle = freq_map[cycle]
+        return df[col_name].groupby(pd.Grouper(freq=cycle, convention="s")).cumcount()+ 1
+
+
+def _get_cycle_names(df: pd.DataFrame, cycle: str):
+    """
+    Gets names of each cycle (year, week etc.) of ``df`` period index
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+    cycle : str, cycle, calendar term ('year', 'quarter', 'month', 'week') or pandas offset string
+
+    Returns
+    -------
+    pd.DataFrame, a column of cycle names
+
+    """
+
+    if cycle == 'year':
+        return df.index.start_time.year
+    elif cycle == 'quarter':
+        return list(map(lambda x: "_".join([str(x.start_time.year), str(x.start_time.quarter)]), df.index))
+    elif cycle == 'month':
+        return list(map(lambda x: "_".join([str(x.start_time.year), str(x.start_time.month)]), df.index))
+    elif cycle == 'quarter':
+        return list(map(_week_of_year, df.index))
+    else:
+        col_name = df.columns[0]
+        return df[col_name].groupby(pd.Grouper(freq=cycle, convention="s")).ngroup()
+
+
+def seasonal_split(df: pd.DataFrame, cycle: str, freq: Optional[str] = None, agg: Union[str, Callable] = "mean") -> pd.DataFrame:
     """
     Converts time series to a DataFrame with columns for each ``cycle`` period.
 
@@ -11,8 +112,8 @@ def seasonal_split(df: pd.DataFrame, cycle: str = "year", freq=None, agg="mean")
     ----------
     df : pd.DataFrame
     cycle : str, cycle, calendar term ('year', 'quarter', 'month', 'week') or pandas offset string
-    freq : frequency, if specified, time series is resampled to it
-    agg : aggregation function used in resampling
+    freq : str, series frequency to resample to
+    agg : str or function, aggregation function used in resampling
 
     Returns
     -------
@@ -20,129 +121,77 @@ def seasonal_split(df: pd.DataFrame, cycle: str = "year", freq=None, agg="mean")
 
     """
 
-    def _week_of_year(t: pd.Period):
-        # pandas weekofyear inconsistency fix, according to ISO week date
-        if t.start_time.weekofyear == 1 and t.start_time.month == 12:
-            year = t.end_time.year
-            week = t.start_time.weekofyear
-        elif t.start_time.weekofyear == 52 and t.end_time.month == 1:
-            year = t.end_time.year - 1
-            week = t.start_time.weekofyear
-        else:
-            year = t.start_time.year
-            week = t.start_time.weekofyear
-        return "_".join([str(year), str(week)])
-
     if freq is None:
         freq = df.index.freqstr
     df = df.resample(freq).agg(agg)
-    col_name = df.columns[0]
 
-    if cycle in ["year", "quarter", "month", "week"]:
-        if cycle == "year":
-            df["_Series"] = df.index.start_time.year
-            if freq == "D":
-                df["_Season"] = df.index.dayofyear
-            elif freq in ["W", "W-SUN"]:
-                df["_Season"] = df.start_time.weekofyear
-            elif freq == "M":
-                df["_Season"] = df.index.start_time.month
-            elif freq in ["Q", "Q-DEC"]:
-                df["_Season"] = df.index.start_time.quarter
-            else:
-                df["_Season"] = (
-                    df[col_name]
-                    .groupby(pd.Grouper(freq="Y", convention="s"))
-                    .cumcount()
-                    + 1
-                )
-
-        elif cycle == "quarter":
-            df["_Series"] = list(
-                map(
-                    lambda x: "_".join(
-                        [str(x.start_time.year), str(x.start_time.quarter)]
-                    ),
-                    df.index,
-                )
-            )
-            df["_Season"] = (
-                df[col_name].groupby(pd.Grouper(freq="Q", convention="s")).cumcount()
-                + 1
-            )
-
-        elif cycle == "month":
-            df["_Series"] = list(
-                map(
-                    lambda x: "_".join(
-                        [str(x.start_time.year), str(x.start_time.month)]
-                    ),
-                    df.index,
-                )
-            )
-            if freq == "D":
-                df["_Season"] = df.index.day
-            else:
-                df["_Season"] = (
-                    df[col_name]
-                    .groupby(pd.Grouper(freq="M", convention="s"))
-                    .cumcount()
-                    + 1
-                )
-
-        elif cycle == "week":
-            df["_Series"] = list(map(_week_of_year, df.index))
-            if freq == "D":
-                df["_Season"] = df.index.dayofweek
-            else:
-                df["_Season"] = (
-                    df[col_name]
-                    .groupby(pd.Grouper(freq="W", convention="s"))
-                    .cumcount()
-                    + 1
-                )
-        else:
-            raise ValueError("Incorrect cycle period name")
-    else:
-        df["_Series"] = (
-            df[col_name].groupby(pd.Grouper(freq=cycle, convention="s")).ngroup()
-        )
-        df["_Season"] = (
-            df[col_name].groupby(pd.Grouper(freq=cycle, convention="s")).cumcount() + 1
-        )
+    df["_Season"] = _get_cycle_names(df, cycle)
+    df["_Series"] = _get_season_names(df, cycle, freq)
 
     return df.set_index(["_Series", "_Season"]).unstack(level=0)
 
 
-def acf(x, max_lags=None):
-
-    """ Autocorrelation estimate function
+def _scale(x: np.array) -> np.array:
+    """
+    Scales x to mean(x) == 0 and std(x) == 1
 
     Parameters
     ----------
-    x : np.array, a time series
-    max_lags : int, maximum number of lags to be calculated
+    x: np.array, array of float to be scaled
 
     Returns
     -------
-    acf : np.array, partial autocorrelations for min(max_lags, len(x)) lags, including lag 0
+    np.array, scaled array
+
     """
 
-    n = x.size
-    if max_lags is None or max_lags > n:
-        max_lags = n
     if np.std(x) == 0:
-        x = x - np.mean(x)
+        return x - np.mean(x)
     else:
-        x = (x - np.mean(x)) / (np.std(x) * np.sqrt(n))
-    if max_lags == n:
-        acf = np.correlate(x, x, mode="full")[-n:]
-    else:
-        acf = np.correlate(x, x, mode="full")[-n : -n + max_lags]
-    return acf
+        return (x - np.mean(x)) / (np.std(x) * np.sqrt(x.size))
 
 
-def yw(x: np.array, order=1, unbiased=False):
+def _autocorrelation(x: np.array) -> np.array:
+    """
+    Autocorrelation via np.correlate for a scaled array `x`
+
+    Parameters
+    ----------
+    x: np.array, input array
+
+    Returns
+    -------
+    np.array, autocorrelation for all lags
+
+    """
+
+    n = len(x)
+    return np.correlate(x, x, mode="full")[-n:] / n
+
+
+def _solve_yw_equation(r: np.array) -> np.array:
+    """
+    Solution to Yule-Walker equations via Töplitz matrix
+
+    Parameters
+    ----------
+    r: autocorrelation coefficients
+
+    Returns
+    -------
+    np.array: partial autocorrelation function
+
+    """
+
+    R = toeplitz(r[:-1])
+    try:
+        return np.linalg.solve(R, r[1:])
+    except np.linalg.LinAlgError:
+        print("Solution is not defined for singular matrices")
+        return [np.nan]
+
+
+def yule_walker(x: np.array, order=1) -> np.array:
 
     """ Estimate ``order`` parameters from a sequence using the Yule-Walker equations.
     http://www-stat.wharton.upenn.edu/~steele/Courses/956/Resource/YWSourceFiles/YW-Eshel.pdf
@@ -161,22 +210,13 @@ def yw(x: np.array, order=1, unbiased=False):
     if order == 0:
         return np.array([1.0])
 
-    n = len(x)
-    r = np.zeros(order + 1, np.float64)
-    r[0] = (x ** 2).sum() / n
-    for k in range(1, order + 1):
-        r[k] = (x[0:-k] * x[k:]).sum() / (n - k * unbiased)
-    R = toeplitz(r[:-1])
-
-    try:
-        rho = np.linalg.solve(R, r[1:])
-    except np.linalg.LinAlgError:
-        print("Solution is not defined for singular matrices")
-        rho = [np.nan]
+    r = _autocorrelation(x)
+    r = r[:order]
+    rho = _solve_yw_equation(r)
     return rho
 
 
-def pacf(x, max_lags: int = 1):
+def pacf(x: np.array, max_lags: Optional[int] = None) -> np.array:
 
     """Partial autocorrelation estimate based on Yule-Walker equations
 
@@ -191,9 +231,30 @@ def pacf(x, max_lags: int = 1):
     """
 
     n = x.size
-    if np.std(x) == 0:
-        x = x - np.mean(x)
-    else:
-        x = (x - np.mean(x)) / (np.std(x) * np.sqrt(n))
-    pacf = np.array([yw(x, i)[-1] for i in range(min(n, max_lags))])
+    if max_lags is None or max_lags > n:
+        max_lags = n
+    x = _scale(x)
+    pacf = np.array([yule_walker(x, i)[-1] for i in range(max_lags)])
     return pacf
+
+
+def acf(x: np.array, max_lags: Optional[int] = None) -> np.array:
+
+    """ Autocorrelation estimate function
+
+    Parameters
+    ----------
+    x : np.array, a time series
+    max_lags : int, maximum number of lags to be calculated
+
+    Returns
+    -------
+    acf : np.array, partial autocorrelations for min(max_lags, len(x)) lags, including lag 0
+    """
+
+    n = x.size
+    if max_lags is None or max_lags > n:
+        max_lags = n
+    x = _scale(x)
+    acf = _autocorrelation(x)
+    return acf[:max_lags]
