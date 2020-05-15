134cf7a532a917548599709788d065ac8f86617a
Simon B <sburyachenko@gmail.com>
2020-05-15 10:27:22 -0300

    Tests import fix

--- gtime/model_selection/__init__.py
+++ gtime/model_selection/__init__.py
@@ -5,5 +5,6 @@ The :mod:`gtime.model_selection` module deals with model selection.
 from .horizon_shift import horizon_shift
 from .splitters import FeatureSplitter
 from .cv_pipeline import CVPipeline
+from .cross_validation import time_series_split, blocking_time_series_split
 
-__all__ = ["FeatureSplitter", "horizon_shift", "CVPipeline"]
+__all__ = ["FeatureSplitter", "horizon_shift", "CVPipeline", "time_series_split", "blocking_time_series_split"]
